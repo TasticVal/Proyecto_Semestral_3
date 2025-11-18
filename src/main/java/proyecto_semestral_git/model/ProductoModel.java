25ef953bee25768d400a5c38c5c7561ef42cebb2
Pablo Figueroa <117122265+bunnybraixen@users.noreply.github.com>
2025-11-17 21:11:32 -0300

    import que faltaron para ProductoModel

--- src/main/java/proyecto_semestral_git/model/ProductoModel.java
+++ src/main/java/proyecto_semestral_git/model/ProductoModel.java
@@ -11,6 +11,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Data
 @NoArgsConstructor
